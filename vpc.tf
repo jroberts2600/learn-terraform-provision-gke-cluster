33a719e580cc60500ae195f13fa85bf663c04617
Jason Roberts <51415896+jroberts2600@users.noreply.github.com>
2021-08-26 13:48:25 -0500

    .

--- vpc.tf
+++ vpc.tf
@@ -9,7 +9,7 @@ variable "region" {
 provider "google" {
   project = var.project_id
   region  = var.region
-  credentials = "${file("CREDENTIALS_FILE.json")}"
+  credentials = var.credentials
 }
 
 # VPC
