9c1d8991995968c7c1404c27341945ee246f1f7c
Jason Roberts <51415896+jroberts2600@users.noreply.github.com>
2021-08-30 16:48:15 -0500

    .

--- gke.tf
+++ gke.tf
@@ -16,7 +16,6 @@ variable "gke_num_nodes" {
 # GKE cluster
 resource "google_container_cluster" "primary" {
   name     = "${var.project_id}-${var.id}-gke"
-  name     = "${var.project_id}-gke-jroberts"
   location = var.region
   
   # We can't create a cluster with no node pool defined, but we want to only use
